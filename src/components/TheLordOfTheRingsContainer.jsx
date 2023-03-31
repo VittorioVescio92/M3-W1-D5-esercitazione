bcea85a21cc2f5042c71d79e1353fda67c93078d
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-03-31 16:54:26 +0200

    +

--- src/components/TheLordOfTheRingsContainer.jsx
+++ src/components/TheLordOfTheRingsContainer.jsx
@@ -10,7 +10,7 @@ class TheLordOfTheRingsContainer extends Component {
         <div>
           <h3 className="text-light">The Lord of the Rings</h3>
         </div>
-        <Row className="mb-5 px-0">
+        <Row id="col" className="mb-5 px-0">
           {movies &&
             movies.map(movie => (
               <Col key={movie.imdbID} xs={12} sm={6} md={4} xl={3} className="g-3">
