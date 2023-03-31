7b6f42352a7176d60139c1f3d7b76c68e42d065a
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-03-31 18:27:21 +0200

    spinner triplicati

--- src/components/MyMain.jsx
+++ src/components/MyMain.jsx
@@ -9,7 +9,9 @@ class MyMain extends Component {
     TheLordOfTheRings: [],
     StarWars: [],
     HarryPotter: [],
-    isLoading: true,
+    StarWarsIsLoading: true,
+    HarryPotterIsLoading: true,
+    TheLordOfTheRingsIsLoading: true,
     StarWarsError: false,
     HarryPotterError: false,
     TheLordOfTheRingsError: false,
@@ -22,7 +24,7 @@ class MyMain extends Component {
       const movies = data.Search.filter(movie => movie.Type === "movie");
       this.setState({ StarWars: movies, isLoading: false });
     } catch (error) {
-      this.setState({ isLoading: false, StarWarsError: true });
+      this.setState({ StarWarsIsLoading: false, StarWarsError: true });
       console.error(error);
     }
 
@@ -32,7 +34,7 @@ class MyMain extends Component {
       const movies = data.Search.filter(movie => movie.Type === "movie");
       this.setState({ HarryPotter: movies, isLoading: false });
     } catch (error) {
-      this.setState({ isLoading: false, HarryPotterError: true });
+      this.setState({ HarryPotterIsLoading: false, HarryPotterError: true });
       console.error(error);
     }
 
@@ -42,7 +44,7 @@ class MyMain extends Component {
       const movies = data.Search.filter(movie => movie.Type === "movie");
       this.setState({ TheLordOfTheRings: movies, isLoading: false });
     } catch (error) {
-      this.setState({ isLoading: false, TheLordOfTheRingsError: true });
+      this.setState({ TheLordOfTheRingsIsLoading: false, TheLordOfTheRingsError: true });
       console.error(error);
     }
   }
@@ -50,19 +52,33 @@ class MyMain extends Component {
   render() {
     return (
       <>
-        {this.state.isLoading && (
-          <div className="text-center mt-5">
-            <Spinner variant="danger" animation="border" role="status" id="spinner">
-              <span className="visually-hidden">Loading...</span>
-            </Spinner>
-          </div>
-        )}
         <Container className="container-fluid justify-content-center px-1">
           <StarWarsContainer movies={this.state.StarWars} />
+          {this.state.StarWarsIsLoading && (
+            <div className="text-center mt-5">
+              <Spinner variant="danger" animation="border" role="status" id="spinner">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            </div>
+          )}
           {this.state.StarWarsError && <Alert variant="danger">Ops! Qualcosa è andato storto...</Alert>}
           <HarryPotterContainer movies={this.state.HarryPotter} />
+          {this.state.HarryPotterIsLoading && (
+            <div className="text-center mt-5">
+              <Spinner variant="danger" animation="border" role="status" id="spinner">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            </div>
+          )}
           {this.state.HarryPotterError && <Alert variant="danger">Ops! Qualcosa è andato storto...</Alert>}
           <TheLordOfTheRingsContainer movies={this.state.TheLordOfTheRings} />
+          {this.state.TheLordOfTheRingsIsLoading && (
+            <div className="text-center mt-5">
+              <Spinner variant="danger" animation="border" role="status" id="spinner">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            </div>
+          )}
           {this.state.TheLordOfTheRingsError && <Alert variant="danger">Ops! Qualcosa è andato storto...</Alert>}
         </Container>
       </>
